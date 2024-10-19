d9bea62a3708cac22187ea14a48e47ecae22622b
Tanishq Jain <tanishq.jain19@gmail.com>
2024-10-19 12:10:35 +0530

    Initial commit

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../interfaces/housing-location';
+import { RouterLink, RouterOutlet } from '@angular/router';
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterOutlet],
+  templateUrl: './housing-location.component.html',
+  styleUrl: './housing-location.component.css'
+})
+
+
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+}
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { provideRouter } from '@angular/router';
+import routeConfig from './app/routes';
+import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+
+
+bootstrapApplication(AppComponent,
+  {
+    providers: [
+      provideProtractorTestingSupport(),
+      provideRouter(routeConfig)
+    ]
+  }
+).catch(err => console.error(err));
